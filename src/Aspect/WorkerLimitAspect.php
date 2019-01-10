555e3454bde04c9621005f5f2a6bd50bff447835
宇润 <admin@yurunsoft.com>
2019-01-10 08:55:18 +0800

    新增并发工作限流器

---
+++ src/Aspect/WorkerLimitAspect.php
@@ -0,0 +1,51 @@
+<?php
+namespace Imi\RateLimit\Aspect;
+
+use Imi\Aop\PointCutType;
+use Imi\Bean\BeanFactory;
+use Imi\Aop\AroundJoinPoint;
+use Imi\Aop\Annotation\Around;
+use Imi\Aop\Annotation\Aspect;
+use Imi\Aop\Annotation\PointCut;
+use Imi\RateLimit\Annotation\WorkerLimit;
+use Imi\Bean\Annotation\AnnotationManager;
+use Imi\RateLimit\Annotation\BlockingConsumer;
+use Imi\RateLimit\WorkerLimiter;
+
+/**
+ * @Aspect
+ */
+class WorkerLimitAspect
+{
+    /**
+     * 处理工作限流
+     * @PointCut(
+     *         type=PointCutType::ANNOTATION,
+     *         allow={
+     *             WorkerLimit::class
+     *         }
+     * )
+     * @Around
+     * @return mixed
+     */
+    public function parse(AroundJoinPoint $joinPoint)
+    {
+        $className = BeanFactory::getObjectClass($joinPoint->getTarget());
+        $method = $joinPoint->getMethod();
+        $workerLimit = AnnotationManager::getMethodAnnotations($className, $method, WorkerLimit::class)[0] ?? null;
+        $blockingConsumer = AnnotationManager::getMethodAnnotations($className, $method, BlockingConsumer::class)[0] ?? null;
+        if(null === $blockingConsumer)
+        {
+            $result = WorkerLimiter::call(function(){
+                $joinPoint->proceed();
+            }, $workerLimit->name, $workerLimit->max, $workerLimit->timeout, $workerLimit->callback, $workerLimit->poolName);
+        }
+        else
+        {
+            $result = WorkerLimiter::callBlock(function(){
+                $joinPoint->proceed();
+            }, $workerLimit->name, $workerLimit->max, $workerLimit->timeout, $blockingConsumer->timeout, $workerLimit->callback, $workerLimit->poolName);
+        }
+        return $result;
+    }
+}
